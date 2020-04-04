4dbf41ad8516391f0ffdc6e7dcafe3f6690bea41
William Antony <williamantony@ymail.com>
2020-04-04 11:10:05 +0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kelasku</title>
+    <link rel="shortcut icon" href="./assets/images/icon.jpg" type="image/x-icon">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="./assets/fontawesome-free-5.12.1-web/css/all.css">
+    <script src="./assets/scripts/jquery-3.4.1.min.js"></script>
+    <script src="script.js"></script>
+</head>
+<body>
+    <nav>
+        <div class="container space-between">
+            <div id="nav-kiri">
+                <div id="logo-kelas">
+                    <img src="./assets/images/icon.jpg" alt="">
+                </div>
+                <div id="menu">
+                    <a href="index.php"><div class="menu-item selected"><i class="fas fa-home"></i> Beranda</div></a>
+                    <a href="index.php"><div class="menu-item"><i class="fas fa-edit"></i> Kumpul Tugas</div></a>
+                    <a href="index.php"><div class="menu-item"><i class="fas fa-book"></i> Materi Kuliah</div></a>
+                    <a href="index.php"><div class="menu-item"><i class="fas fa-chair"></i> Cek Kursi</div></a>
+                    <a href="index.php"><div class="menu-item"><i class="fas fa-calendar"></i> Acara Kelas</div></a>
+                </div>
+            </div>
+            <div id="nav-kanan">
+                <div id="nama">
+                    William Antony
+                    <div id="dropdown-nav-kanan">
+                        <a href="index.php"><div class="dropdown-nav-kanan-item"><i class="fas fa-user"></i> Ubah Profil</div></a>
+                        <a href="index.php"><div class="dropdown-nav-kanan-item"><i class="fas fa-sign-out-alt"></i> Keluar</div></a>
+                    </div>
+                </div>
+                <div id="profil">
+                    <img src="./assets/images/people.jpg" alt="">
+                </div>
+            </div>
+        </div>
+    </nav>
+</body>
+</html>
---
+++ script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $("#nama").click(function(){
+        $("#dropdown-nav-kanan").toggle();
+    });
+    $("#profil").click(function(){
+        $("#dropdown-nav-kanan").toggle();
+    });
+});
