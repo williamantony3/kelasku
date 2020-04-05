2d75a5973087e4104832d08e761a5c0ae2eff86c
William Antony <williamantony@ymail.com>
2020-04-05 12:03:32 +0700

    updated design

--- index.php
+++ index.php
@@ -8,9 +8,25 @@
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="./assets/fontawesome-free-5.12.1-web/css/all.css">
     <script src="./assets/scripts/jquery-3.4.1.min.js"></script>
-    <script src="script.js"></script>
+    <script>
+        function startTime() {
+            var today = new Date();
+            var h = today.getHours();
+            var m = today.getMinutes();
+            var s = today.getSeconds();
+            m = checkTime(m);
+            s = checkTime(s);
+            document.getElementById('jam').innerHTML =
+            h + ":" + m + ":" + s;
+            var t = setTimeout(startTime, 500);
+        }
+        function checkTime(i) {
+            if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+            return i;
+        }
+    </script>
 </head>
-<body>
+<body onload="startTime()">
     <nav>
         <div class="container space-between">
             <div id="nav-kiri">
@@ -39,5 +55,144 @@
             </div>
         </div>
     </nav>
+    <div class="container">
+        <div id="sidebar-kiri">
+            <div id="ucapan">
+                <div id="ucapan-orang">
+                    Halo, William Antony!
+                </div>
+                <div id="status-absen">
+                    <span class="belum-absen">Belum Absen</span>
+                </div>
+                <div id="data-diri">
+                    <div class="data-diri-header">NIM</div>
+                    2201827673
+                    <div class="data-diri-header">E-mail</div>
+                    william.antony001@binus.ac.id
+                </div>
+            </div>
+            <div id="status-kelas">
+                <div id="jumlah-orang">
+                    3
+                </div>
+                temanmu belum absen
+                <a href="index.php"><div id="siapa-aja"><i class="fas fa-eye"></i> Siapa tuh?</div></a>
+            </div>
+        </div>
+        <div id="postingan-utama">
+            <div class="postingan-item">
+                <div class="yang-post">
+                    <div class="foto-ts">
+                        <img src="./assets/images/people.jpg" alt="" srcset="">
+                    </div>
+                    <div class="yang-post-kanan">
+                        <div class="nama-ts">
+                            William Antony
+                        </div>
+                        <div class="waktu-post">
+                            17:09, 4 April 2020
+                        </div>
+                    </div>
+                </div>
+                <div class="isi-post">
+                    Gais ada materi baru nih gais!
+                </div>
+                <a href=""><div class="materi-item"><i class="fas fa-file"></i> Materi.pdf</div></a>
+            </div><div class="postingan-item">
+                <div class="yang-post">
+                    <div class="foto-ts">
+                        <img src="./assets/images/people.jpg" alt="" srcset="">
+                    </div>
+                    <div class="yang-post-kanan">
+                        <div class="nama-ts">
+                            William Antony
+                        </div>
+                        <div class="waktu-post">
+                            17:09, 4 April 2020
+                        </div>
+                    </div>
+                </div>
+                <div class="isi-post">
+                    Gais ada materi baru nih gais!
+                </div>
+                <a href=""><div class="materi-item"><i class="fas fa-file"></i> Materi.pdf</div></a>
+            </div><div class="postingan-item">
+                <div class="yang-post">
+                    <div class="foto-ts">
+                        <img src="./assets/images/people.jpg" alt="" srcset="">
+                    </div>
+                    <div class="yang-post-kanan">
+                        <div class="nama-ts">
+                            William Antony
+                        </div>
+                        <div class="waktu-post">
+                            17:09, 4 April 2020
+                        </div>
+                    </div>
+                </div>
+                <div class="isi-post">
+                    Gais ada materi baru nih gais!
+                </div>
+                <a href=""><div class="materi-item"><i class="fas fa-file"></i> Materi.pdf</div></a>
+            </div><div class="postingan-item">
+                <div class="yang-post">
+                    <div class="foto-ts">
+                        <img src="./assets/images/people.jpg" alt="" srcset="">
+                    </div>
+                    <div class="yang-post-kanan">
+                        <div class="nama-ts">
+                            William Antony
+                        </div>
+                        <div class="waktu-post">
+                            17:09, 4 April 2020
+                        </div>
+                    </div>
+                </div>
+                <div class="isi-post">
+                    Gais ada materi baru nih gais!
+                </div>
+                <a href=""><div class="materi-item"><i class="fas fa-file"></i> Materi.pdf</div></a>
+            </div>
+            
+            <footer>
+                    2020 &copy; CIH!
+            </footer>
+        </div>
+        <div id="sidebar-kanan">
+            <div id="waktu">
+                <div id="tanggal"></div>
+                <div id="jam"></div>
+                
+                <div class="acara-item">
+                    <div class="acara-kiri">
+                        <i class="fas fa-calendar"></i>
+                    </div>
+                    <div class="acara-kanan">
+                        <div class="judul-acara">
+                            Tukar Kado
+                        </div>
+                        <div class="detail-acara">
+                            17:30 WIB, Ruang A801
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="acara-item">
+                    <div class="acara-kiri">
+                        <i class="fas fa-calendar"></i>
+                    </div>
+                    <div class="acara-kanan">
+                        <div class="judul-acara">
+                            Tukar Kado
+                        </div>
+                        <div class="detail-acara">
+                            17:30 WIB, Ruang A801
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="script.js"></script>
 </body>
 </html>
--- script.js
+++ script.js
@@ -6,3 +6,7 @@ $(document).ready(function(){
         $("#dropdown-nav-kanan").toggle();
     });
 });
+var d = new Date();
+var hari = ["Minggu", "Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu"]
+var bulan = ["Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember"];
+document.getElementById("tanggal").innerHTML = hari[d.getDay()] + ", " + d.getDate() + " " + bulan[d.getMonth()] + " " + d.getFullYear();
