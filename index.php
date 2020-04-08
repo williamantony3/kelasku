360d61535d24aecaec75b48cdbd1e6d65b7a2d45
William Antony <williamantony@ymail.com>
2020-04-09 00:04:09 +0700

    login

--- index.php
+++ index.php
@@ -1,60 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Kelasku</title>
-    <link rel="shortcut icon" href="./assets/images/icon.jpg" type="image/x-icon">
-    <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="./assets/fontawesome-free-5.12.1-web/css/all.css">
-    <script src="./assets/scripts/jquery-3.4.1.min.js"></script>
-    <script>
-        function startTime() {
-            var today = new Date();
-            var h = today.getHours();
-            var m = today.getMinutes();
-            var s = today.getSeconds();
-            m = checkTime(m);
-            s = checkTime(s);
-            document.getElementById('jam').innerHTML =
-            h + ":" + m + ":" + s;
-            var t = setTimeout(startTime, 500);
-        }
-        function checkTime(i) {
-            if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
-            return i;
-        }
-    </script>
-</head>
-<body onload="startTime()">
-    <nav>
-        <div class="container space-between">
-            <div id="nav-kiri">
-                <div id="logo-kelas">
-                    <img src="./assets/images/icon.jpg" alt="">
-                </div>
-                <div id="menu">
-                    <a href="index.php"><div class="menu-item selected"><i class="fas fa-home"></i> Beranda</div></a>
-                    <a href="index.php"><div class="menu-item"><i class="fas fa-edit"></i> Kumpul Tugas</div></a>
-                    <a href="index.php"><div class="menu-item"><i class="fas fa-book"></i> Materi Kuliah</div></a>
-                    <a href="index.php"><div class="menu-item"><i class="fas fa-chair"></i> Cek Kursi</div></a>
-                    <a href="index.php"><div class="menu-item"><i class="fas fa-calendar"></i> Acara Kelas</div></a>
-                </div>
-            </div>
-            <div id="nav-kanan">
-                <div id="nama">
-                    William Antony
-                    <div id="dropdown-nav-kanan">
-                        <a href="index.php"><div class="dropdown-nav-kanan-item"><i class="fas fa-user"></i> Ubah Profil</div></a>
-                        <a href="index.php"><div class="dropdown-nav-kanan-item"><i class="fas fa-sign-out-alt"></i> Keluar</div></a>
-                    </div>
-                </div>
-                <div id="profil">
-                    <img src="./assets/images/people.jpg" alt="">
-                </div>
-            </div>
-        </div>
-    </nav>
+<?php include "header.php"; ?>
     <div class="container">
         <div id="sidebar-kiri">
             <div id="ucapan">
@@ -193,6 +137,4 @@
             </div>
         </div>
     </div>
-    <script src="script.js"></script>
-</body>
-</html>
+<?php include "footer.php"; ?>
---
+++ login.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Masuk | Kelasku</title>
+    <link rel="shortcut icon" href="./assets/images/icon.jpg" type="image/x-icon">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="./assets/fontawesome-free-5.12.1-web/css/all.css">
+    <script src="./assets/scripts/jquery-3.4.1.min.js"></script>
+</head>
+<body>
+    <div id="login-page">
+        <div id="login-page-kiri">
+            <h3>Yang Masih Belum Absen</h3>
+            <table>
+                <thead>
+                    <tr>
+                        <th>NO</th>
+                        <th>NAMA</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>Harry Wijaya</td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>Kelvin Chandra</td>
+                    </tr>
+                    <tr>
+                        <td>3</td>
+                        <td>Verina Armelia</td>
+                    </tr>
+                    <tr>
+                        <td>4</td>
+                        <td>Vincent Gunawan</td>
+                    </tr>
+                    <tr>
+                        <td>5</td>
+                        <td>William Antony</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <div id="login-page-kanan">
+            <h2>Masuk Kelasku</h2>
+            <form action="" method="post">
+                <div class="grup-input">
+                    <input type="text" name="username" id="" required="required" placeholder="Nama Pengguna">
+                </div>
+                <div class="grup-input">
+                    <input type="password" name="password" id="" required="required" placeholder="Kata Sandi">
+                </div>
+                <div class="grup-input">
+                    <button type="submit">Masuk</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
